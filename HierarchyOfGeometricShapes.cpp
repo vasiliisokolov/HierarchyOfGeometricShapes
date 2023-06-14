e358abde7a89f6cb55d8a364d90a3b4f58bd92ea
Basileus <Basileus@DESKTOP-M14F8KM>
2023-06-14 20:58:39 +0300

    fst step

--- HierarchyOfGeometricShapes.cpp
+++ HierarchyOfGeometricShapes.cpp
@@ -1,7 +1,18 @@
 ﻿#include <iostream>
 
+enum operation
+{
+    ex, 
+    circle, 
+    square, 
+    triangle, 
+    rectangle
+};
+
 class Figure
 {
+public:
+
     enum color
     {
         none = 0,
@@ -12,10 +23,58 @@ class Figure
     int cX;
     int cY;
     color c;
+};
 
+class Circle : public Figure
+{
+
+public:
+
+    double radius;
+
+    double area()
+    {
+        return atan(1) * 4 * radius;
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+
+    double edge;
+
+    double area()
+    {
+        return edge * edge * std::sqrt(3) / 4;
+    }
+};
+
+class Square : public Figure
+{
+public:
+
+    double edge;
+
+    double area()
+    {
+        return edge * edge;
+    }
+};
+
+class Rectangle : public Figure
+{
+public:
+
+    double width, height;
+
+    double area()
+    {
+        return width * height;
+    }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "";
 }
