246ead8bbbff785ccdd316d8e5a57775b1e4f901
Basileus <Basileus@DESKTOP-M14F8KM>
2023-06-14 13:51:58 +0300

    Добавьте файлы проекта.

---
+++ HierarchyOfGeometricShapes.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+class Figure
+{
+    enum color
+    {
+        none = 0,
+        red = 1,
+        blue = 2,
+        green = 3
+    };
+    int cX;
+    int cY;
+    color c;
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
