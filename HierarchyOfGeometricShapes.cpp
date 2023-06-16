153cabc4c754dfeadfefa1f633d661a70bc91be9
Basileus <Basileus@DESKTOP-M14F8KM>
2023-06-16 21:51:02 +0300

    subClass

--- HierarchyOfGeometricShapes.cpp
+++ HierarchyOfGeometricShapes.cpp
@@ -36,6 +36,18 @@ public:
     {
         return atan(1) * 4 * radius;
     }
+
+    double describe()
+    {
+        return radius * radius;
+    }
+
+    Circle(int inX, int inY, color inC, double inRadius): radius(inRadius)
+    {
+        cX = inX;
+        cY = inY;
+        c = inC;
+    }
 };
 
 class Triangle : public Figure
@@ -47,6 +59,18 @@ public:
     double area()
     {
         return edge * edge * std::sqrt(3) / 4;
+    }
+    
+    double describe()
+    {
+        return edge * edge;
+    }
+
+    Triangle (int inX, int inY, color inC, double inEdge) : edge(inEdge)
+    {
+        cX = inX;
+        cY = inY;
+        c = inC;
     }
 };
 
@@ -60,6 +84,18 @@ public:
     {
         return edge * edge;
     }
+
+    double describe()
+    {
+        return edge * edge;
+    }
+
+    Square (int inX, int inY, color inC, double inEdge) : edge(inEdge)
+    {
+        cX = inX;
+        cY = inY;
+        c = inC;
+    }
 };
 
 class Rectangle : public Figure
@@ -72,9 +108,55 @@ public:
     {
         return width * height;
     }
+
+    double describe()
+    {
+        return width * height;
+    }
+
+    Rectangle (int inX, int inY, color inC, double inWidth, double inHeight) : width(inWidth), height(inHeight)
+    {
+        cX = inX;
+        cY = inY;
+        c = inC;
+    }
+
 };
 
 int main()
 {
-    std::cout << "";
+    int input;
+    std::cout << "Choose the shape: ";
+    std::cin >> input;
+
+    while (true)
+    {
+        std::cout << "Choose the shape: ";
+        std::cin >> input;
+        if (input > 4 || input < 0)
+        {
+            std::cerr << "Error! Wrong operation! Try again!" << std::endl;
+        }
+        else
+        {
+            switch (input)
+            {
+            case operation::circle:
+                break;
+            case operation::square:
+                
+                break;
+            case operation::triangle:
+                
+                break;
+            case operation::rectangle:
+
+                break;
+            case operation::ex:
+
+                return 0;
+
+            }
+        }
+    }
 }
